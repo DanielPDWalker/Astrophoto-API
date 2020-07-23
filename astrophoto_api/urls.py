6420c20c8987e9fcea20c2f3ec4bc5f1bf528e1e
danielpdwalker <danielpdwalker@gmail.com>
2020-07-23 14:03:40 +0100

    Added a media url to the project urls.py

--- astrophoto_api/urls.py
+++ astrophoto_api/urls.py
@@ -15,13 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import url
+from django.views.static import serve
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('messier_objects.urls'))
+    path('', include('messier_objects.urls')),
+    url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT,})
 ]
 
 urlpatterns += [
-    path('api-auth/', include('rest_framework.urls')),
+    path('api-auth/', include('rest_framework.urls'))
 ]
 
