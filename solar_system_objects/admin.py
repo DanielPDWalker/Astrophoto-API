fb84d346042db0e28a1041f725e7a4011e0c239b
danielpdwalker <danielpdwalker@gmail.com>
2020-07-28 14:33:45 +0100

    Added more details about models to admin area

--- solar_system_objects/admin.py
+++ solar_system_objects/admin.py
@@ -3,4 +3,14 @@ from django.contrib import admin
 from solar_system_objects import models
 
 
-admin.site.register(models.SolarSystemObject)
+class SolarSystemObjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'object_type',
+                    'parent_object', 'distance_from_sun_au', 'captured')
+    list_display_links = ('name',)
+    list_editable = ('captured',)
+    search_fields = ('object_type', 'parent_object',
+                     'distance_from_sun_au')
+    list_per_page = 25
+
+
+admin.site.register(models.SolarSystemObject, SolarSystemObjectAdmin)
