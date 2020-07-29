679fb779e3b2c384c76c88bf20d876db2d6ffc3d
danielpdwalker <danielpdwalker@gmail.com>
2020-07-29 16:52:16 +0100

    Started to add testing for frontend

---
+++ frontend/tests/test_urls.py
@@ -0,0 +1,23 @@
+from django.test import SimpleTestCase
+from django.urls import reverse, resolve
+from frontend.views import mes_obj_overview, mes_obj_detail
+from frontend.views import sol_obj_overview, sol_obj_detail
+
+
+class TestUrls(SimpleTestCase):
+
+    def test_messier_objects_url_resolves(self):
+        url = reverse('mes_obj_overview')
+        self.assertEquals(resolve(url).func, mes_obj_overview)
+
+    def test_messier_objects_detail_url_resolves(self):
+        # The detail url takes a parameter.
+        url = reverse('mes_obj_detail', args=[1])
+        self.assertEquals(resolve(url).func, mes_obj_detail)
+
+    def test_solar_system_object_url_resolves(self):
+        pass
+
+    def test_solar_system_object_detail_url_resolves(self):
+        # The detail url takes a parameter.
+        pass
