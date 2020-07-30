e30952813d19e8a596fa9e26484a4f9f7c1860bf
danielpdwalker <danielpdwalker@gmail.com>
2020-07-30 14:17:47 +0100

    Adding front end testing. URLs done Views WIP

--- frontend/tests/test_urls.py
+++ frontend/tests/test_urls.py
@@ -2,6 +2,7 @@ from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from frontend.views import mes_obj_overview, mes_obj_detail
 from frontend.views import sol_obj_overview, sol_obj_detail
+from frontend.views import acm_obj_overview, acm_obj_detail
 
 
 class TestUrls(SimpleTestCase):
@@ -16,8 +17,19 @@ class TestUrls(SimpleTestCase):
         self.assertEquals(resolve(url).func, mes_obj_detail)
 
     def test_solar_system_object_url_resolves(self):
-        pass
+        url = reverse('sol_obj_overview')
+        self.assertEquals(resolve(url).func, sol_obj_overview)
 
     def test_solar_system_object_detail_url_resolves(self):
         # The detail url takes a parameter.
-        pass
+        url = reverse('sol_obj_detail', args=[1])
+        self.assertEquals(resolve(url).func, sol_obj_detail)
+
+    def test_acm_object_url_resolves(self):
+        url = reverse('acm_obj_overview')
+        self.assertEquals(resolve(url).func, acm_obj_overview)
+
+    def test_acm_object_detail_url_resolves(self):
+        # The detail url takes a parameter.
+        url = reverse('acm_obj_detail', args=[1])
+        self.assertEquals(resolve(url).func, acm_obj_detail)
---
+++ frontend/tests/test_views.py
@@ -0,0 +1,49 @@
+import json
+from django.test import TestCase, Client
+from django.urls import reverse
+
+from asteroids_comets_meteors.models import AsteroidCometMeteorObject
+from messier_objects.models import MessierObject
+from solar_system_objects.models import SolarSystemObject
+
+
+class TestViews(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+
+        # Messier Object Set Up
+        self.mes_overview_url = reverse('mes_obj_overview')
+        self.messier_obj_test = MessierObject.objects.create(
+            messier_number=1000000,
+            name='test1000000',
+            ncg_or_ic_number=1000000,
+            object_type='Test',
+            distance_kly=25,
+            constellation='Test',
+            apparent_magnitude=1000000,
+        )
+        self.mes_detail_url = reverse('mes_obj_detail', args=[
+                                      self.messier_obj_test.messier_number])
+        # Solar System Object Set Up
+        self.sol_overview_url = reverse('sol_obj_overview')
+        self.sol_obj_test = SolarSystemObject.objects.create(
+            name='test',
+            object_type='test',
+            parent_object='test',
+            distance_from_sun_au=0,
+        )
+        self.sol_detail_url = reverse('sol_obj_detail', args=[
+                                      self.sol_obj_test.slug])
+
+    def test_mes_obj_overview_get(self):
+        response = self.client.get(self.mes_overview_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, 'api_frontends/messier_objects_overview.html')
+
+    def test_mes_obj_detail_get(self):
+        response = self.client.get(self.mes_detail_url)
+        self.assertEquals(response.status_code, 200)
+        self.assertTemplateUsed(
+            response, 'api_frontends/messier_object_detail.html')
